034b3da2c735355a300f72bfe0647d294a4a1219
KimGeunBeom <59364681+agfalcon@users.noreply.github.com>
2023-02-15 13:44:30 +0900

    [성공]암호 만들기 // -자음과 모음을 나눠서 리스트에 받음 -리스트에서 원하는 개수만큼 뽑을 수 있는 조합 알고리즘 사용 -자음과 모음에서 일정 개수를 뽑아서 리스트에 삽입 -모든 경우를 리스트에 저장 후 정렬 과정을 거치고 출력

--- src/재귀/b_1759.java
+++ src/재귀/b_1759.java
@@ -2,7 +2,9 @@ package 재귀;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.CookieHandler;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
@@ -25,21 +27,69 @@ public class b_1759 {
             }
         }
         StringBuilder sb = new StringBuilder();
-        ArrayList<Character> result = new ArrayList<>();
-        for(int i=2;L-i>=1;i++){
-            result.clear();
-            getAlpha(result, i, parent);
-            getAlpha(result, L-i, child);
-            Collections.sort(result);
-            for(int j=0;j<result.size();j++){
-                sb.append(result.get(j));
+
+        ArrayList<char[]> result_child = new ArrayList<>();
+        Collections.sort(child);
+        Collections.sort(parent);
+        boolean visited_c[] = new boolean[child.size()];
+        boolean visited_b[] = new boolean[parent.size()];
+        ArrayList<char[]> result_parent = new ArrayList<>();
+        ArrayList<String> result = new ArrayList<>();
+        for(int i=2;L-i>=1 && i<=child.size();i++){
+            result_parent.clear();
+            result_child.clear();
+            getAlpha(result_parent, L-i, L-i, 0, parent, visited_b);
+            getAlpha(result_child, i, i, 0,child, visited_c);
+            for(int c=0;c<result_child.size();c++){
+                for(int p=0;p<result_parent.size();p++){
+                    char[] temp = new char[result_child.get(c).length + result_parent.get(p).length];
+                    System.arraycopy(result_child.get(c), 0, temp, 0, result_child.get(c).length);
+                    System.arraycopy(result_parent.get(p), 0, temp, result_child.get(c).length, result_parent.get(p).length);
+                    Arrays.sort(temp);
+                    String s = "";
+                    for(int t=0;t<temp.length;t++){
+                        s = s + temp[t];
+                    }
+                    if(!result.contains(s)){
+                        result.add(s);
+                    }
+                }
             }
-            sb.append("\n");
+        }
+        Collections.sort(result);
+        for(int i=0;i<result.size();i++){
+            sb.append(result.get(i)).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void getAlpha(ArrayList result, int t, int r, int depth, ArrayList list, boolean visited[]){
+        if(r==0){
+            save(list, t, visited, result);
+            return;
+        }
+        if(depth==list.size()){
+            return;
+        }
+        else{
+            visited[depth] = true;
+            getAlpha(result, t, r-1, depth +1,  list, visited);
+
+            visited[depth] = false;
+            getAlpha(result,t, r, depth +1, list, visited);
         }
     }
 
-    private static void getAlpha(ArrayList result, int num, ArrayList list){
-
+    private static void save(ArrayList<Character> list, int r, boolean[] visited, ArrayList<char[]> result){
+        char[] temp = new char[r];
+        int t = 0;
+        for(int i=0;i<list.size();i++){
+            if(visited[i]){
+                temp[t] = list.get(i);
+                t++;
+            }
+        }
+        result.add(temp);
     }
 
     private static boolean isChild(char input){
